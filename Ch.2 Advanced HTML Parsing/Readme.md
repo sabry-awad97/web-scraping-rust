50c802cfb658bf38be9b29b0a0897b576e5f84ab
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-27 20:30:46 +0300

    🐛 fix(Readme.md, src/main.rs): Correct previous sibling text extraction logic
    📝 **Readme.md:**
    - Added an example of dealing with parent elements and extracting previous sibling text using the `scraper` library in Rust.
    
    📝 **src/main.rs:**
    - Removed the unnecessary `iterate_table_next_siblings` function.
    - Replaced the usage of `iterate_table_next_siblings` with `find_previous_sibling_text` for consistency.
    
    ✨ **src/main.rs:**
    - Added an example to demonstrate how to find and print previous sibling text of an image element using the `scraper` library.
    
    This commit improves the code logic for extracting previous sibling text from an HTML element. The unnecessary function `iterate_table_next_siblings` was removed, and the `find_previous_sibling_text` function was updated to handle the image element scenario.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- Ch.2 Advanced HTML Parsing/Readme.md
+++ Ch.2 Advanced HTML Parsing/Readme.md
@@ -420,3 +420,48 @@ fn iterate_table_next_siblings(html: &str, table_id: &str) -> Vec<String> {
     }
 }
 ```
+
+## Dealing with parents
+
+```rs
+use scraper::{ElementRef, Html, Selector};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "http://www.pythonscraping.com/pages/page3.html";
+    let html = fetch_html(url).await?;
+
+    let img_src = "../img/gifts/img1.jpg";
+    if let Some(text) = find_previous_sibling_text(&html, img_src) {
+        println!("{}", text);
+    } else {
+        println!("Previous sibling text not found.");
+    }
+
+    Ok(())
+}
+
+async fn fetch_html(url: &str) -> Result<String, reqwest::Error> {
+    let response = reqwest::get(url).await?;
+    let body = response.text().await?;
+    Ok(body)
+}
+
+fn find_previous_sibling_text(html: &str, img_src: &str) -> Option<String> {
+    let document = Html::parse_document(html);
+    let img_selector = Selector::parse(&format!("img[src='{}']", img_src)).unwrap();
+
+    if let Some(img_element) = document.select(&img_selector).next() {
+        if let Some(prev_sibling) = img_element
+            .parent()
+            .and_then(|parent| parent.prev_sibling())
+            .and_then(ElementRef::wrap)
+        {
+            return Some(prev_sibling.text().collect::<String>());
+        }
+    }
+
+    None
+}
+```
--- Ch.2 Advanced HTML Parsing/src/main.rs
+++ Ch.2 Advanced HTML Parsing/src/main.rs
@@ -6,10 +6,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let url = "http://www.pythonscraping.com/pages/page3.html";
     let html = fetch_html(url).await?;
 
-    let table_id = "giftList";
-    let siblings = iterate_table_next_siblings(&html, table_id);
-    for sibling in siblings {
-        println!("{}", sibling);
+    let img_src = "../img/gifts/img1.jpg";
+    if let Some(text) = find_previous_sibling_text(&html, img_src) {
+        println!("{}", text);
+    } else {
+        println!("Previous sibling text not found.");
     }
 
     Ok(())
@@ -21,26 +22,19 @@ async fn fetch_html(url: &str) -> Result<String, reqwest::Error> {
     Ok(body)
 }
 
-fn iterate_table_next_siblings(html: &str, table_id: &str) -> Vec<String> {
+fn find_previous_sibling_text(html: &str, img_src: &str) -> Option<String> {
     let document = Html::parse_document(html);
-    let selector = Selector::parse(&format!("#{} tr", table_id)).unwrap();
+    let img_selector = Selector::parse(&format!("img[src='{}']", img_src)).unwrap();
 
-    if let Some(table_row) = document.select(&selector).next() {
-        let siblings: Vec<String> = table_row
-            .next_siblings()
-            .filter_map(ElementRef::wrap)
-            .map(|element| {
-                let selector = Selector::parse("td").unwrap();
-                let cells = element.select(&selector).skip(1);
-                cells
-                    .map(|cell| cell.text().collect::<String>())
-                    .collect::<Vec<_>>()
-                    .join("\t")
-            })
-            .collect();
-
-        siblings
-    } else {
-        Vec::new()
+    if let Some(img_element) = document.select(&img_selector).next() {
+        if let Some(prev_sibling) = img_element
+            .parent()
+            .and_then(|parent| parent.prev_sibling())
+            .and_then(ElementRef::wrap)
+        {
+            return Some(prev_sibling.text().collect::<String>());
+        }
     }
+
+    None
 }
