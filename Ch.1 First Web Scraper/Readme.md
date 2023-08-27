c7c8f1903712661f24655e413be9aa1b4cba4e1f
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-27 15:14:23 +0300

    🎉 feat(Ch.1): Add reliable connection and exception handling in web scraping code
    This commit introduces improvements to the web scraping code to establish a reliable connection and handle exceptions more effectively. It enhances the codebase to handle both successful and unsuccessful HTTP responses gracefully.
    
    **Changes Made:**
    - Added a new code block in `Readme.md` that demonstrates connecting reliably to a URL and handling exceptions using the `reqwest` crate.
    - Modified the `src/main.rs` file to reflect the changes made in the `Readme.md` example.
    - Updated the main function in `src/main.rs` to use the updated error handling and connection logic, similar to the example in `Readme.md`.
    
    **Description:**
    In the `Readme.md` file, a new section titled "Connecting Reliably and Handling Exceptions" was added. This section includes a code example that showcases how to establish a reliable connection to a URL using the `reqwest` crate. It demonstrates handling both successful and unsuccessful HTTP responses and provides appropriate error messages for different scenarios.
    
    In the `src/main.rs` file, the existing code was modified to match the example provided in the "Connecting Reliably and Handling Exceptions" section of the `Readme.md` file. The code now uses the `Client` from the `reqwest` crate to make an HTTP request, check the response status, and handle both successful and unsuccessful responses appropriately.
    
    **Impact:**
    These changes enhance the robustness of the web scraping code by ensuring reliable connections and effective exception handling. Developers using this code will benefit from a more stable scraping experience and improved error reporting.
    
    **Signed-off-by:** Sabry Awad <dr.sabry1997@gmail.com>

--- Ch.1 First Web Scraper/Readme.md
+++ Ch.1 First Web Scraper/Readme.md
@@ -126,3 +126,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 ```
+
+## Connecting Reliably and Handling Exceptions
+
+```rs
+use reqwest::Client;
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+
+    let url = "http://pythonscraping.com/pages/page1.html";
+
+    match client.get(url).send().await {
+        Ok(response) => {
+            if response.status().is_success() {
+                match response.text().await {
+                    Ok(body) => {
+                        println!("Body: {}", body);
+                    }
+                    Err(err) => {
+                        eprintln!("Error reading response body: {}", err);
+                    }
+                }
+            } else {
+                eprintln!("Request was not successful: {:?}", response.status());
+            }
+        }
+        Err(err) => {
+            eprintln!("Error sending HTTP request: {}", err);
+        }
+    };
+}
+```
--- Ch.1 First Web Scraper/src/main.rs
+++ Ch.1 First Web Scraper/src/main.rs
@@ -1,24 +1,28 @@
 use reqwest::Client;
-use scraper::{Html, Selector};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     let client = Client::new();
-    let response = client
-        .get("http://www.pythonscraping.com/pages/page1.html")
-        .send()
-        .await?
-        .text()
-        .await?;
 
-    let document = Html::parse_document(&response);
+    let url = "http://pythonscraping.com/pages/page1.html";
 
-    let h1_selector = Selector::parse("h1").unwrap();
-    if let Some(h1_element) = document.select(&h1_selector).next() {
-        println!("{}", h1_element.text().collect::<String>());
-    } else {
-        println!("No <h1> element found.");
-    }
-
-    Ok(())
+    match client.get(url).send().await {
+        Ok(response) => {
+            if response.status().is_success() {
+                match response.text().await {
+                    Ok(body) => {
+                        println!("Body: {}", body);
+                    }
+                    Err(err) => {
+                        eprintln!("Error reading response body: {}", err);
+                    }
+                }
+            } else {
+                eprintln!("Request was not successful: {:?}", response.status());
+            }
+        }
+        Err(err) => {
+            eprintln!("Error sending HTTP request: {}", err);
+        }
+    };
 }
