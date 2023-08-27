09dcbda78355146bd2c9f58906544c35c9cec247
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-27 21:13:33 +0300

    🎉 feat(Ch.2 Advanced HTML Parsing): Implement dynamic element search using closures
    Implemented a new functionality that allows for searching HTML elements based on a closure's condition. This feature enhances the HTML scraping capabilities of the application.
    
    Changes made:
    - Added a new Rust file `main.rs` in the `Ch.2 Advanced HTML Parsing/src` directory.
    - Replaced the previous image source regex-based search with the new dynamic HTML element search using closures.
    - Defined a closure-based function `find_elements_with_closure` which takes HTML content and a closure as parameters to find matching elements.
    - Utilized the `scraper` crate to parse the HTML document and select elements based on the provided closure's condition.
    - Modified the main function in `Ch.2 Advanced HTML Parsing` to demonstrate the usage of the new dynamic element search functionality.
    
    This change enhances the flexibility of element searching and allows for more complex conditions when locating elements in HTML documents.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- Ch.2 Advanced HTML Parsing/Readme.md
+++ Ch.2 Advanced HTML Parsing/Readme.md
@@ -512,3 +512,47 @@ fn find_images_with_src_regex(html: &str, img_src_regex: &str) -> Vec<String> {
     src_attributes
 }
 ```
+
+## Dynamic HTML Element Search using Closures
+
+```rs
+use scraper::{Html, Selector};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "http://www.pythonscraping.com/pages/page3.html";
+    let html = fetch_html(url).await?;
+
+    let target_text = "Or maybe he's only resting?";
+    let matching_elements = find_elements_with_closure(&html, |element| {
+        element.text().collect::<String>() == target_text
+    });
+
+    for element in matching_elements {
+        println!("{}", element);
+    }
+
+    Ok(())
+}
+
+async fn fetch_html(url: &str) -> Result<String, reqwest::Error> {
+    let response = reqwest::get(url).await?;
+    let body = response.text().await?;
+    Ok(body)
+}
+
+fn find_elements_with_closure<F>(html: &str, closure: F) -> Vec<String>
+where
+    F: Fn(&scraper::ElementRef) -> bool,
+{
+    let document = Html::parse_document(html);
+    let matching_elements: Vec<String> = document
+        .select(&Selector::parse("*").unwrap())
+        .filter(|element| closure(element))
+        .map(|element| element.text().collect())
+        .collect();
+
+    matching_elements
+}
+```
--- Ch.2 Advanced HTML Parsing/src/main.rs
+++ Ch.2 Advanced HTML Parsing/src/main.rs
@@ -1,4 +1,3 @@
-use regex::Regex;
 use scraper::{Html, Selector};
 use std::error::Error;
 
@@ -7,10 +6,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let url = "http://www.pythonscraping.com/pages/page3.html";
     let html = fetch_html(url).await?;
 
-    let img_src_regex = r"\.\.\/img\/gifts\/img.*\.jpg";
-    let src_attributes = find_images_with_src_regex(&html, img_src_regex);
-    for src in src_attributes {
-        println!("{}", src);
+    let target_text = "Or maybe he's only resting?";
+    let matching_elements = find_elements_with_closure(&html, |element| {
+        element.text().collect::<String>() == target_text
+    });
+
+    for element in matching_elements {
+        println!("{}", element);
     }
 
     Ok(())
@@ -22,17 +24,16 @@ async fn fetch_html(url: &str) -> Result<String, reqwest::Error> {
     Ok(body)
 }
 
-fn find_images_with_src_regex(html: &str, img_src_regex: &str) -> Vec<String> {
+fn find_elements_with_closure<F>(html: &str, closure: F) -> Vec<String>
+where
+    F: Fn(&scraper::ElementRef) -> bool,
+{
     let document = Html::parse_document(html);
-    let img_selector = Selector::parse("img[src]").unwrap();
-    let regex = Regex::new(img_src_regex).unwrap();
-
-    let src_attributes: Vec<String> = document
-        .select(&img_selector)
-        .filter_map(|element| element.value().attr("src"))
-        .filter(|src| regex.is_match(src))
-        .map(String::from)
+    let matching_elements: Vec<String> = document
+        .select(&Selector::parse("*").unwrap())
+        .filter(|element| closure(element))
+        .map(|element| element.text().collect())
         .collect();
 
-    src_attributes
+    matching_elements
 }
