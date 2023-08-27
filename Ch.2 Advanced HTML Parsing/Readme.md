5a9ead7e3db1d610886c5ddcb22fb6c64040f48a
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-27 18:22:07 +0300

    🎉 feat(Ch.2): Add example for extracting elements by ID 🎉 feat(Ch.2): Refactor main.rs to use ID-based element extraction In order to showcase the extraction of HTML elements by their IDs, a new code snippet has been added to the `Readme.md` file in the "Ch.2 Advanced HTML Parsing" directory. The new example demonstrates how to extract elements using the `scraper` library based on their IDs. Additionally, the existing code in the `src/main.rs` file has been modified to use the same ID-based extraction technique, resulting in cleaner and more consistent code. Changes made: - Added a new section in the `Readme.md` file that demonstrates extracting elements using IDs. - Refactored the code in `src/main.rs` to use the `find_elements_by_id` function instead of the previous class-based selector approach. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- Ch.2 Advanced HTML Parsing/Readme.md
+++ Ch.2 Advanced HTML Parsing/Readme.md
@@ -183,3 +183,41 @@ fn count_text_occurrences(html: &str, target_text: &str) -> usize {
     count
 }
 ```
+
+## Extracting with Id
+
+```rs
+use scraper::{Html, Selector};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "http://www.pythonscraping.com/pages/warandpeace.html";
+    let html = fetch_html(url).await?;
+
+    let elements = find_elements_by_id(&html, "text");
+    for element in elements {
+        println!("{}", element);
+    }
+
+    Ok(())
+}
+
+async fn fetch_html(url: &str) -> Result<String, reqwest::Error> {
+    let response = reqwest::get(url).await?;
+    let body = response.text().await?;
+    Ok(body)
+}
+
+fn find_elements_by_id(html: &str, id_value: &str) -> Vec<String> {
+    let document = Html::parse_document(html);
+    let selector = Selector::parse(&format!("#{}", id_value)).unwrap();
+
+    let elements: Vec<String> = document
+        .select(&selector)
+        .map(|element| element.text().collect())
+        .collect();
+
+    elements
+}
+```
--- Ch.2 Advanced HTML Parsing/src/main.rs
+++ Ch.2 Advanced HTML Parsing/src/main.rs
@@ -6,9 +6,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let url = "http://www.pythonscraping.com/pages/warandpeace.html";
     let html = fetch_html(url).await?;
 
-    let spans = find_all_tags_with_classes(&html, "span", &["green", "red"]);
-    for span in spans {
-        println!("{}", span);
+    let elements = find_elements_by_id(&html, "text");
+    for element in elements {
+        println!("{}", element);
     }
 
     Ok(())
@@ -20,26 +20,14 @@ async fn fetch_html(url: &str) -> Result<String, reqwest::Error> {
     Ok(body)
 }
 
-fn find_all_tags_with_classes(html: &str, tag: &str, class_names: &[&str]) -> Vec<String> {
+fn find_elements_by_id(html: &str, id_value: &str) -> Vec<String> {
     let document = Html::parse_document(html);
-    let selector = create_selector_for_maybe_classes(tag, class_names);
-    let spans: Vec<String> = document
+    let selector = Selector::parse(&format!("#{}", id_value)).unwrap();
+
+    let elements: Vec<String> = document
         .select(&selector)
         .map(|element| element.text().collect())
         .collect();
 
-    spans
-}
-
-fn create_selector_for_maybe_classes(element_name: &str, class_names: &[&str]) -> Selector {
-    let mut css_selector = String::from(element_name);
-    for (index, class) in class_names.iter().enumerate() {
-        if index == 0 {
-            css_selector.push_str(&format!(".{}", class));
-        } else {
-            css_selector.push_str(&format!(", .{}", class));
-        }
-    }
-
-    Selector::parse(&css_selector).unwrap()
+    elements
 }
