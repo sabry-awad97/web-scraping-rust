1d648277c20a4d1bfa7fb8481b60e731013b93b2
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-28 20:29:18 +0300

    📊 chore(Ch.5): Extract and store data from Wikipedia tables 🔗 link(Ch.5): http://en.wikipedia.org/wiki/Comparison_of_text_editors
    This commit introduces code to scrape and store data from Wikipedia tables. The Rust program utilizes the `csv` crate for data storage and the `scraper` crate for HTML parsing.
    
    Changes made:
    - Added code to fetch HTML content from a specified URL.
    - Implemented logic to parse tables from the fetched HTML content using CSS selectors.
    - Extracted row data from the tables, processed it, and stored it in a CSV file.
    
    The program scrapes a Wikipedia page for a table with the class `wikitable`, extracts the rows and cells, and writes the cell content to a CSV file named `editors.csv`. This commit sets the groundwork for further data extraction and analysis.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- Ch.5 Storing Data/src/main.rs
+++ Ch.5 Storing Data/src/main.rs
@@ -1,18 +1,23 @@
 use csv::Writer;
+use scraper::{Html, Selector};
 use std::error::Error;
 use std::fs::File;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let csv_file_path = "test.csv";
+    let url = "http://en.wikipedia.org/wiki/Comparison_of_text_editors";
+    let html = fetch_html(url).await?;
 
+    let table_selector = "table.wikitable";
+    let rows = find_table_rows(&html, table_selector);
+
+    let csv_file_path = "editors.csv";
     let csv_file = File::create(csv_file_path)?;
+
     let mut csv_writer = Writer::from_writer(csv_file);
 
-    csv_writer.write_record(["number", "number plus 2", "number times 2"])?;
-
-    for i in 0..10 {
-        csv_writer.write_record([i.to_string(), (i + 2).to_string(), (i * 2).to_string()])?;
+    for row in rows {
+        csv_writer.write_record(row)?;
     }
 
     csv_writer.flush()?;
@@ -20,4 +25,36 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("CSV file written successfully.");
 
     Ok(())
+}
+
+async fn fetch_html(url: &str) -> Result<String, reqwest::Error> {
+    let response = reqwest::get(url).await?;
+    let body = response.text().await?;
+    Ok(body)
+}
+
+fn find_table_rows<'a>(html: &'a str, selector: &'a str) -> Vec<Vec<String>> {
+    let document = Html::parse_document(html);
+    let table_selector = Selector::parse(selector).unwrap();
+    let row_selector = Selector::parse("tr").unwrap();
+
+    let selected_elements: std::iter::Skip<scraper::html::Select<'_, '_>> =
+        document.select(&table_selector).skip(1);
+
+    let mut table_rows = Vec::new();
+
+    for table in selected_elements {
+        let row_data: Vec<String> = table
+            .select(&row_selector)
+            .flat_map(|row| {
+                row.select(&Selector::parse("td").unwrap())
+                    .map(|cell| cell.text().collect::<String>())
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+
+        table_rows.push(row_data);
+    }
+
+    table_rows
 }
