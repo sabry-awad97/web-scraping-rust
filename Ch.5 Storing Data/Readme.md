3b26f04b4f43670d487bf1bd236240ad6fb2d762
Sabry Awad <dr.sabry1997@gmail.com>
2023-09-01 14:22:18 +0300

    🔧 fix(.gitignore): Add .env file to ignore list ✨ feat(Cargo.toml): Add async-trait and lettre dependencies 📝 **Summary:** Added .env file to the ignore list in .gitignore to prevent it from being tracked. Also, added async-trait and lettre dependencies to the Cargo.toml file for use in the project. 💡 **Details**: - Included `.env` in the `.gitignore` file to ensure that it is not tracked by the VCS. - Added `async-trait` and `lettre` dependencies in the `Cargo.toml` file to enable the use of their functionalities. 📚 **Context**: - The `.env` file typically contains sensitive information such as API keys or database credentials, which should not be committed to the VCS. - The `async-trait` crate provides traits that are useful for working with asynchronous code in Rust. - The `lettre` crate is a fast and simple SMTP library for sending emails in Rust. 🔧 Changes: - Added `.env` to the `.gitignore`

--- Ch.5 Storing Data/Readme.md
+++ Ch.5 Storing Data/Readme.md
@@ -324,3 +324,135 @@ fn main() -> Result<(), mysql::Error> {
     Ok(())
 }
 ```
+
+## Sending Email
+
+```rs
+use async_trait::async_trait;
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::transport::smtp::response::Response;
+use lettre::{Message, SmtpTransport, Transport};
+use scraper::{Html, Selector};
+use std::thread::sleep;
+use std::time::Duration;
+
+#[derive(thiserror::Error, Debug)]
+enum AppError {
+    #[error("Email error: {0}")]
+    Letter(#[from] lettre::error::Error),
+
+    #[error("Address error: {0}")]
+    Address(#[from] lettre::address::AddressError),
+
+    #[error("SMTP error: {0}")]
+    Smtp(#[from] lettre::transport::smtp::Error),
+
+    #[error("HTTP error: {0}")]
+    Http(#[from] reqwest::Error),
+
+    #[error("Selector error: {0}")]
+    Selector(#[from] scraper::error::SelectorErrorKind<'static>),
+}
+
+// Define a trait for email sending
+trait EmailSender {
+    fn send_email(
+        &self,
+        from_email: &str,
+        to_email: &str,
+        subject: &str,
+        body: &str,
+    ) -> Result<Response, AppError>;
+}
+
+// Implement the EmailSender trait for SmtpTransport
+impl EmailSender for SmtpTransport {
+    fn send_email(
+        &self,
+        from_email: &str,
+        to_email: &str,
+        subject: &str,
+        body: &str,
+    ) -> Result<Response, AppError> {
+        let email = Message::builder()
+            .from(from_email.parse()?)
+            .to(to_email.parse()?)
+            .subject(subject)
+            .header(ContentType::TEXT_PLAIN)
+            .body(body.to_owned())?;
+
+        Ok(self.send(&email)?)
+    }
+}
+
+#[async_trait]
+trait WebScraper {
+    async fn fetch_html(&self, url: &str) -> Result<String, AppError>;
+}
+
+// Implement the WebScraper trait using reqwest
+struct ReqwestWebScraper;
+
+#[async_trait]
+impl WebScraper for ReqwestWebScraper {
+    async fn fetch_html(&self, url: &str) -> Result<String, AppError> {
+        let response = reqwest::get(url).await?;
+        let html = response.text().await?;
+        Ok(html)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), AppError> {
+    // URL of the website to check
+    let url = "https://isitchristmas.com/";
+
+    // SMTP credentials
+    let smtp_username = "your_smtp_username";
+    let smtp_password = "your_smtp_password";
+
+    // Email configuration
+    let from_email = "from_email@example.com";
+    let to_email = "to_email@example.com";
+    let email_subject = "It's Christmas!";
+    let email_body = "According to http://itischristmas.com, it is Christmas!";
+
+    // SMTP server configuration (e.g., for Gmail)
+    let smtp_server = "smtp.gmail.com";
+    let smtp_port = 587;
+
+    let email_transport = SmtpTransport::relay(smtp_server)?
+        .port(smtp_port)
+        .credentials(Credentials::new(
+            smtp_username.to_owned(),
+            smtp_password.to_owned(),
+        ))
+        .build();
+
+    let web_scraper = ReqwestWebScraper;
+    let answer_selector = Selector::parse("a#answer")?;
+
+    // Check if it's Christmas and send email
+    loop {
+        let html = web_scraper.fetch_html(url).await?;
+
+        // Parse the HTML using the scraper crate
+        let document = Html::parse_document(&html);
+
+        if let Some(answer_node) = document.select(&answer_selector).next() {
+            let answer = answer_node.value().attr("title").unwrap_or_default();
+
+            if answer == "YES" {
+                email_transport.send_email(from_email, to_email, email_subject, email_body)?;
+                break;
+            }
+        }
+
+        // Sleep for an hour before checking again
+        sleep(Duration::from_secs(3600));
+    }
+
+    Ok(())
+}
+```
--- Ch.5 Storing Data/src/main.rs
+++ Ch.5 Storing Data/src/main.rs
@@ -1,36 +1,127 @@
-use std::env;
+use async_trait::async_trait;
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::transport::smtp::response::Response;
+use lettre::{Message, SmtpTransport, Transport};
+use scraper::{Html, Selector};
+use std::thread::sleep;
+use std::time::Duration;
 
-use dotenvy::dotenv;
-use mysql::prelude::*;
+#[derive(thiserror::Error, Debug)]
+enum AppError {
+    #[error("Email error: {0}")]
+    Letter(#[from] lettre::error::Error),
 
-fn main() -> Result<(), mysql::Error> {
-    dotenv().ok();
+    #[error("Address error: {0}")]
+    Address(#[from] lettre::address::AddressError),
 
-    let db_user = env::var("DB_USER").expect("DB_USER not set");
-    let db_pass = env::var("DB_PASS").expect("DB_PASS not set");
+    #[error("SMTP error: {0}")]
+    Smtp(#[from] lettre::transport::smtp::Error),
 
-    let mut opts = mysql::OptsBuilder::new();
-    opts = opts.user(Some(db_user)).pass(Some(db_pass));
+    #[error("HTTP error: {0}")]
+    Http(#[from] reqwest::Error),
 
-    let pool = mysql::Pool::new(opts)?;
+    #[error("Selector error: {0}")]
+    Selector(#[from] scraper::error::SelectorErrorKind<'static>),
+}
 
-    let mut conn = pool.get_conn()?;
+// Define a trait for email sending
+trait EmailSender {
+    fn send_email(
+        &self,
+        from_email: &str,
+        to_email: &str,
+        subject: &str,
+        body: &str,
+    ) -> Result<Response, AppError>;
+}
 
-    conn.query_drop("DROP DATABASE IF EXISTS scraping")?;
-    conn.query_drop("CREATE DATABASE scraping")?;
-    conn.query_drop("USE scraping")?;
+// Implement the EmailSender trait for SmtpTransport
+impl EmailSender for SmtpTransport {
+    fn send_email(
+        &self,
+        from_email: &str,
+        to_email: &str,
+        subject: &str,
+        body: &str,
+    ) -> Result<Response, AppError> {
+        let email = Message::builder()
+            .from(from_email.parse()?)
+            .to(to_email.parse()?)
+            .subject(subject)
+            .header(ContentType::TEXT_PLAIN)
+            .body(body.to_owned())?;
 
-    conn.query_drop(
-        r#"
-        CREATE TABLE pages (
-            id BIGINT NOT NULL AUTO_INCREMENT,
-            title VARCHAR(200),
-            content VARCHAR(10000),
-            created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            PRIMARY KEY(id)
-        )
-    "#,
-    )?;
+        Ok(self.send(&email)?)
+    }
+}
+
+#[async_trait]
+trait WebScraper {
+    async fn fetch_html(&self, url: &str) -> Result<String, AppError>;
+}
+
+// Implement the WebScraper trait using reqwest
+struct ReqwestWebScraper;
+
+#[async_trait]
+impl WebScraper for ReqwestWebScraper {
+    async fn fetch_html(&self, url: &str) -> Result<String, AppError> {
+        let response = reqwest::get(url).await?;
+        let html = response.text().await?;
+        Ok(html)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), AppError> {
+    // URL of the website to check
+    let url = "https://isitchristmas.com/";
+
+    // SMTP credentials
+    let smtp_username = "your_smtp_username";
+    let smtp_password = "your_smtp_password";
+
+    // Email configuration
+    let from_email = "from_email@example.com";
+    let to_email = "to_email@example.com";
+    let email_subject = "It's Christmas!";
+    let email_body = "According to http://itischristmas.com, it is Christmas!";
+
+    // SMTP server configuration (e.g., for Gmail)
+    let smtp_server = "smtp.gmail.com";
+    let smtp_port = 587;
+
+    let email_transport = SmtpTransport::relay(smtp_server)?
+        .port(smtp_port)
+        .credentials(Credentials::new(
+            smtp_username.to_owned(),
+            smtp_password.to_owned(),
+        ))
+        .build();
+
+    let web_scraper = ReqwestWebScraper;
+    let answer_selector = Selector::parse("a#answer")?;
+
+    // Check if it's Christmas and send email
+    loop {
+        let html = web_scraper.fetch_html(url).await?;
+
+        // Parse the HTML using the scraper crate
+        let document = Html::parse_document(&html);
+
+        if let Some(answer_node) = document.select(&answer_selector).next() {
+            let answer = answer_node.value().attr("title").unwrap_or_default();
+
+            if answer == "YES" {
+                email_transport.send_email(from_email, to_email, email_subject, email_body)?;
+                break;
+            }
+        }
+
+        // Sleep for an hour before checking again
+        sleep(Duration::from_secs(3600));
+    }
 
     Ok(())
 }
