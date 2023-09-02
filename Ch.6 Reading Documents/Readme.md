884205340a6b6581eb25414d9fb64995fc4542bb
Sabry Awad <dr.sabry1997@gmail.com>
2023-09-02 08:45:28 +0300

    📝 **Summary:** - Removed unused code related to PDF fetching and text extraction. - Added code to fetch a Word document and read the content of 'word/document.xml' from the document. 💡 **Details**: - Removed the `PdfFetcher` trait, `PdfUrlFetcher` struct, and associated implementation. This code was not being used and was unrelated to the Word document fetching. - Removed the `PdfTextExtractorService` trait and `PdfExtractor` struct. This code was also unused and unrelated to the Word document fetching. - Added code to fetch a Word document from a URL using `reqwest` and read the content of 'word/document.xml' from the document using the `zip` crate. 📚 **Context**: - The changes remove unused and unrelated code for PDF fetching and text extraction. Instead, the code now focuses on fetching and reading the content of a Word document. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- Ch.6 Reading Documents/Readme.md
+++ Ch.6 Reading Documents/Readme.md
@@ -302,3 +302,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 ```
+
+## Reading Text Content from a Word Document
+
+```rs
+use std::io::{self, Read};
+use zip::ZipArchive;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Fetch the Word document
+    let url = "http://pythonscraping.com/pages/AWordDocument.docx";
+    let response = reqwest::get(url).await?;
+    let word_file = response.bytes().await?;
+
+    // Wrap the Word file in a Read cursor
+    let cursor = io::Cursor::new(word_file);
+
+    // Open the Word document as a Zip archive
+    let mut archive = ZipArchive::new(cursor)?;
+
+    // Read the content of 'word/document.xml'
+    for i in 0..archive.len() {
+        let mut entry = archive.by_index(i)?;
+        if entry.name() == "word/document.xml" {
+            let mut xml_content = String::new();
+            entry.read_to_string(&mut xml_content)?;
+            println!("{}", xml_content);
+            break;
+        }
+    }
+
+    Ok(())
+}
+```
--- Ch.6 Reading Documents/src/main.rs
+++ Ch.6 Reading Documents/src/main.rs
@@ -1,60 +1,29 @@
-use async_trait::async_trait;
-use reqwest::Client;
-use std::error::Error;
-
-// Define a trait for PDF fetching
-#[async_trait]
-trait PdfFetcher {
-    async fn fetch_pdf(&self, url: &str) -> Result<Vec<u8>, Box<dyn Error>>;
-}
-
-// Implement the PdfFetcher trait for a specific PDF fetcher
-struct PdfUrlFetcher {
-    client: Client,
-}
-
-impl PdfUrlFetcher {
-    fn new() -> Self {
-        let client = Client::new();
-        Self { client }
-    }
-}
-
-#[async_trait]
-impl PdfFetcher for PdfUrlFetcher {
-    async fn fetch_pdf(&self, url: &str) -> Result<Vec<u8>, Box<dyn Error>> {
-        // Fetch the PDF content using reqwest
-        let pdf_response = self.client.get(url).send().await?;
-        let pdf_bytes = pdf_response.bytes().await?;
-        Ok(pdf_bytes.to_vec())
-    }
-}
-
-// Define a trait for PDF text extraction
-trait PdfTextExtractorService {
-    fn extract_text(&self, pdf_data: &[u8]) -> Result<String, Box<dyn Error>>;
-}
-
-// Implement the PdfTextExtractorService trait for a specific extractor
-struct PdfExtractor;
-
-impl PdfTextExtractorService for PdfExtractor {
-    fn extract_text(&self, pdf_data: &[u8]) -> Result<String, Box<dyn Error>> {
-        let pdf_text = pdf_extract::extract_text_from_mem(pdf_data)?;
-
-        Ok(pdf_text)
-    }
-}
+use std::io::{self, Read};
+use zip::ZipArchive;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let pdf_url = "http://pythonscraping.com/pages/warandpeace/chapter1.pdf";
-    let pdf_fetcher = PdfUrlFetcher::new();
-    let pdf_data = pdf_fetcher.fetch_pdf(pdf_url).await?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Fetch the Word document
+    let url = "http://pythonscraping.com/pages/AWordDocument.docx";
+    let response = reqwest::get(url).await?;
+    let word_file = response.bytes().await?;
 
-    let pdf_extractor = PdfExtractor;
-    let pdf_text = pdf_extractor.extract_text(&pdf_data)?;
+    // Wrap the Word file in a Read cursor
+    let cursor = io::Cursor::new(word_file);
 
-    println!("{}", pdf_text);
+    // Open the Word document as a Zip archive
+    let mut archive = ZipArchive::new(cursor)?;
+
+    // Read the content of 'word/document.xml'
+    for i in 0..archive.len() {
+        let mut entry = archive.by_index(i)?;
+        if entry.name() == "word/document.xml" {
+            let mut xml_content = String::new();
+            entry.read_to_string(&mut xml_content)?;
+            println!("{}", xml_content);
+            break;
+        }
+    }
+
     Ok(())
 }
