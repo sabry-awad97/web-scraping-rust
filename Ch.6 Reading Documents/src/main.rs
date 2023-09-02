bb3d55d8223ad858216f4716f5c7746b2256099e
Sabry Awad <dr.sabry1997@gmail.com>
2023-09-02 07:20:12 +0300

    🐛 fix(main.rs): Fix UTF-8 decoding error and update URL ✨ feat(main.rs): Retrieve text content as bytes instead of string 📝 **Summary:** Fixed an issue with UTF-8 decoding error and updated the URL to fetch text content. Modified the code to retrieve the text content as bytes instead of a string. 💡 **Details**: - Added `FromUtf8Error` to handle the UTF-8 decoding error that may occur during the conversion from bytes to string. - Replaced `response.text().await?` with `response.bytes().await?` to fetch the text content as bytes. - Changed the URL to `"http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt"` to update the text content being fetched. 📚 **Context**: - Previously, there was a decoding error related to the conversion from bytes to a string. This issue has been resolved by incorporating the `FromUtf8Error` type and appropriate error handling. - Additionally, the URL for fetching the text content has been updated to `"http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt"`. 🐛 Bug Fix: - The problem involved a UTF-8 decoding error when converting text content from bytes to a string. This issue has been resolved by incorporating `FromUtf8Error` and implementing error handling. ✨ Feature Enhancement: - The code has been modified to retrieve the text content as bytes instead of a string. This approach allows for greater flexibility in handling and processing the fetched data. 🚀 Challenges: - No significant challenges were encountered during the implementation of these changes. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- Ch.6 Reading Documents/src/main.rs
+++ Ch.6 Reading Documents/src/main.rs
@@ -1,9 +1,14 @@
+use std::string::FromUtf8Error;
+
 use async_trait::async_trait;
 
 #[derive(thiserror::Error, Debug)]
 enum AppError {
     #[error("HTTP request error: {0}")]
     HttpRequest(#[from] reqwest::Error),
+
+    #[error("UTF-8 decoding error: {0}")]
+    Utf8Decoding(#[from] FromUtf8Error),
 }
 
 // WebScraper trait for fetching HTML
@@ -18,15 +23,16 @@ struct ReqwestWebScraper;
 impl WebScraper for ReqwestWebScraper {
     async fn fetch_html(&self, url: &str) -> Result<String, AppError> {
         let response = reqwest::get(url).await?;
-        let html = response.text().await?;
-        Ok(html)
+        let bytes = response.bytes().await?;
+        let text_content = String::from_utf8(bytes.to_vec())?;
+        Ok(text_content)
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
     let web_scraper = ReqwestWebScraper;
-    let text_url = "http://www.pythonscraping.com/pages/warandpeace/chapter1.txt";
+    let text_url = "http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt";
 
     let text_content = web_scraper.fetch_html(text_url).await?;
 
