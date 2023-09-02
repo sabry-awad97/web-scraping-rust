42601da9062049aad924daa150a188e40956a7b8
Sabry Awad <dr.sabry1997@gmail.com>
2023-09-02 09:47:44 +0300

    🎉 feat(Cargo.toml): Add quick-xml dependency 🎉 feat(src/main.rs): Extract text from Word document using quick-xml
    📝 **Summary:**
    - Added the quick-xml dependency to Cargo.toml to handle XML parsing.
    - Modified src/main.rs to extract text from a Word document using quick-xml.
    
    💡 **Details**:
    - Added the "quick-xml" package and its version to the dependencies in Cargo.toml.
    - In src/main.rs:
      - Imported the necessary modules from quick-xml.
      - Defined a reader and set trim_text to true to eliminate leading/trailing white space.
      - Created a buffer and boolean value to track the state of being inside a "w:t" element.
      - Extracted the content of the "word/document.xml" file from the Word document.
      - Used a loop to iterate through the XML events and process the necessary elements.
      - Appended the text content of "w:t" elements to a string variable, extracted_text.
      - Printed the extracted_text to the console.
    
    📚 **Context**:
    - The Word document at the specified URL contains XML content within the "word/document.xml" file. The goal is to extract the text content from this file.
    - Utilizing the quick-xml dependency simplifies the XML parsing process.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- Ch.6 Reading Documents/src/main.rs
+++ Ch.6 Reading Documents/src/main.rs
@@ -1,11 +1,13 @@
+use quick_xml::events::Event;
+use quick_xml::reader::Reader;
 use std::io::{self, Read};
 use zip::ZipArchive;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Fetch the Word document
-    let url = "http://pythonscraping.com/pages/AWordDocument.docx";
-    let response = reqwest::get(url).await?;
+    let docx_url = "http://pythonscraping.com/pages/AWordDocument.docx";
+    let response = reqwest::get(docx_url).await?;
     let word_file = response.bytes().await?;
 
     // Wrap the Word file in a Read cursor
@@ -15,15 +17,49 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut archive = ZipArchive::new(cursor)?;
 
     // Read the content of 'word/document.xml'
+    let mut xml_content = String::new();
+
     for i in 0..archive.len() {
         let mut entry = archive.by_index(i)?;
         if entry.name() == "word/document.xml" {
-            let mut xml_content = String::new();
             entry.read_to_string(&mut xml_content)?;
-            println!("{}", xml_content);
             break;
         }
     }
+
+    let mut reader = Reader::from_str(&xml_content);
+    reader.trim_text(true);
+
+    let mut buf = Vec::new();
+    let mut inside_w_t = false;
+
+    let mut extracted_text = String::new();
+    loop {
+        match reader.read_event_into(&mut buf) {
+            Ok(Event::Start(ref e)) => {
+                if e.name().as_ref() == b"w:t" {
+                    inside_w_t = true;
+                }
+            }
+            Ok(Event::Text(e)) => {
+                if inside_w_t {
+                    let text = &e.unescape()?;
+                    extracted_text.push_str(text);
+                }
+            }
+            Ok(Event::End(ref e)) => {
+                if e.name().as_ref() == b"w:t" {
+                    inside_w_t = false;
+                }
+            }
+            Ok(Event::Eof) => break,
+            _ => {}
+        }
+        
+        buf.clear();
+    }
+
+    println!("{}", extracted_text);
 
     Ok(())
 }
