f59e325d3f28e39256ce4d001f92916d2945f5cd
Sabry Awad <dr.sabry1997@gmail.com>
2023-09-02 07:44:02 +0300

    📝 **Summary:** - Added CSV dependency to `Cargo.toml` in order to read CSV files. - Updated `main.rs` to implement CSV parsing functionality. - Created a `HtmlFetcher` trait and a corresponding implementation using the `ReqwestHtmlFetcher` struct. - Introduced a `CsvParser` trait and a `CsvReader` struct to handle CSV parsing. - Modified the `main` function to fetch HTML and parse CSV data. 💡 **Details**: - Added `csv = "1.2.2"` to the dependencies in `Cargo.toml` to enable CSV parsing. - Created a `CsvParser` trait and a `CsvReader` struct to handle CSV parsing. - Implemented the `HtmlFetcher` trait using the `ReqwestHtmlFetcher` struct to fetch HTML content. - Updated the `main` function to fetch HTML content and parse the CSV data. ✨ Feature Enhancement: - Added CSV parsing functionality to the project. - Introduced a `CsvParser` trait and a `CsvReader` struct to handle CSV parsing. 🚀 Challenges: - No specific challenges were encountered during the implementation of these changes. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- Ch.6 Reading Documents/Readme.md
+++ Ch.6 Reading Documents/Readme.md
@@ -66,3 +66,83 @@ fn main() {
     println!("{}", cleaned_text);
 }
 ```
+
+## Reading CSV
+
+```rs
+use async_trait::async_trait;
+use csv::{Error as CsvError, ReaderBuilder};
+use reqwest::{Client, Error as ReqwestError};
+use std::string::FromUtf8Error;
+
+#[derive(thiserror::Error, Debug)]
+enum AppError {
+    #[error("HTTP request error: {0}")]
+    HttpRequest(#[from] ReqwestError),
+
+    #[error("UTF-8 decoding error: {0}")]
+    Utf8Decoding(#[from] FromUtf8Error),
+
+    #[error("CSV error: {0}")]
+    Csv(#[from] CsvError),
+}
+
+#[async_trait]
+trait HtmlFetcher {
+    async fn fetch_html(&self, url: &str) -> Result<String, AppError>;
+}
+
+struct ReqwestHtmlFetcher {
+    client: Client,
+}
+
+impl ReqwestHtmlFetcher {
+    fn new() -> Self {
+        let client = Client::new();
+        Self { client }
+    }
+}
+
+#[async_trait]
+impl HtmlFetcher for ReqwestHtmlFetcher {
+    async fn fetch_html(&self, url: &str) -> Result<String, AppError> {
+        let response = self.client.get(url).send().await?;
+        let text_content = response.text().await?;
+        Ok(text_content)
+    }
+}
+
+trait CsvParser {
+    fn parse_csv(&self, csv_text: &str) -> Result<(), AppError>;
+}
+
+struct CsvReader;
+
+impl CsvParser for CsvReader {
+    fn parse_csv(&self, csv_text: &str) -> Result<(), AppError> {
+        let mut csv_reader = ReaderBuilder::new().from_reader(csv_text.as_bytes());
+
+        for result in csv_reader.records() {
+            let record = result?;
+            for field in record.iter() {
+                print!("{} | ", field);
+            }
+            println!();
+        }
+
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), AppError> {
+    let html_fetcher = ReqwestHtmlFetcher::new();
+    let csv_parser = CsvReader;
+    let csv_url = "http://pythonscraping.com/files/MontyPythonAlbums.csv";
+
+    let csv_text = html_fetcher.fetch_html(csv_url).await?;
+    csv_parser.parse_csv(&csv_text)?;
+
+    Ok(())
+}
+```
--- Ch.6 Reading Documents/src/main.rs
+++ Ch.6 Reading Documents/src/main.rs
@@ -1,41 +1,75 @@
-use std::string::FromUtf8Error;
-
 use async_trait::async_trait;
+use csv::{Error as CsvError, ReaderBuilder};
+use reqwest::{Client, Error as ReqwestError};
+use std::string::FromUtf8Error;
 
 #[derive(thiserror::Error, Debug)]
 enum AppError {
     #[error("HTTP request error: {0}")]
-    HttpRequest(#[from] reqwest::Error),
+    HttpRequest(#[from] ReqwestError),
 
     #[error("UTF-8 decoding error: {0}")]
     Utf8Decoding(#[from] FromUtf8Error),
+
+    #[error("CSV error: {0}")]
+    Csv(#[from] CsvError),
 }
 
-// WebScraper trait for fetching HTML
 #[async_trait]
-trait WebScraper {
+trait HtmlFetcher {
     async fn fetch_html(&self, url: &str) -> Result<String, AppError>;
 }
 
-struct ReqwestWebScraper;
+struct ReqwestHtmlFetcher {
+    client: Client,
+}
+
+impl ReqwestHtmlFetcher {
+    fn new() -> Self {
+        let client = Client::new();
+        Self { client }
+    }
+}
 
 #[async_trait]
-impl WebScraper for ReqwestWebScraper {
+impl HtmlFetcher for ReqwestHtmlFetcher {
     async fn fetch_html(&self, url: &str) -> Result<String, AppError> {
-        let response = reqwest::get(url).await?;
-        let bytes = response.bytes().await?;
-        let text_content = String::from_utf8(bytes.to_vec())?;
+        let response = self.client.get(url).send().await?;
+        let text_content = response.text().await?;
         Ok(text_content)
+    }
+}
+
+trait CsvParser {
+    fn parse_csv(&self, csv_text: &str) -> Result<(), AppError>;
+}
+
+struct CsvReader;
+
+impl CsvParser for CsvReader {
+    fn parse_csv(&self, csv_text: &str) -> Result<(), AppError> {
+        let mut csv_reader = ReaderBuilder::new().from_reader(csv_text.as_bytes());
+
+        for result in csv_reader.records() {
+            let record = result?;
+            for field in record.iter() {
+                print!("{} | ", field);
+            }
+            println!();
+        }
+
+        Ok(())
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
-    let web_scraper = ReqwestWebScraper;
-    let text_url = "http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt";
+    let html_fetcher = ReqwestHtmlFetcher::new();
+    let csv_parser = CsvReader;
+    let csv_url = "http://pythonscraping.com/files/MontyPythonAlbums.csv";
 
-    let text_content = web_scraper.fetch_html(text_url).await?;
+    let csv_text = html_fetcher.fetch_html(csv_url).await?;
+    csv_parser.parse_csv(&csv_text)?;
 
-    println!("{}", text_content);
     Ok(())
 }
