7c49285d404009adf55e939cd984e3072eea9b0c
Sabry Awad <dr.sabry1997@gmail.com>
2023-09-02 08:24:53 +0300

    🎉 feat(Cargo.toml): Add pdf-extract crate dependency 🐛 fix(main.rs): Update code to reflect changes in dependencies and trait names 📝 **Summary**: - Added the `pdf-extract` crate as a dependency in the `Cargo.toml` file. - Updated the code in `main.rs` to reflect changes in dependencies and trait names. 💡 **Details**: - Added `pdf-extract = "0.7.0"` to the dependencies in the `Cargo.toml` file. - Renamed the `HtmlFetcher` trait to `PdfFetcher`. - Changed the implementation of the `PdfFetcher` trait for `PdfUrlFetcher`. - Renamed the `CsvParser` trait to `PdfTextExtractorService`. - Implemented the `PdfTextExtractorService` trait for `PdfExtractor`. 📚 **Context**: - The `PdfFetcher` trait and associated code were originally implemented with the name `HtmlFetcher`. This change was made to better reflect the purpose of the trait and its implementations. - The crate `pdf-extract` was added as a dependency to enable PDF text extraction functionality. 🐛 Bug Fix: - N/A ✨ Feature Enhancement: - Added the `pdf-extract` crate as a dependency to enable PDF text extraction. 🚀 Challenges: - N/A Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- Ch.6 Reading Documents/src/main.rs
+++ Ch.6 Reading Documents/src/main.rs
@@ -1,35 +1,19 @@
 use async_trait::async_trait;
-use csv::{Error as CsvError, ReaderBuilder};
-use reqwest::{Client, Error as ReqwestError};
-use std::collections::HashMap;
-use std::io::Cursor;
-use std::string::FromUtf8Error;
+use reqwest::Client;
+use std::error::Error;
 
-#[derive(thiserror::Error, Debug)]
-enum AppError {
-    #[error("HTTP request error: {0}")]
-    HttpRequest(#[from] ReqwestError),
-
-    #[error("UTF-8 decoding error: {0}")]
-    Utf8Decoding(#[from] FromUtf8Error),
-
-    #[error("CSV error: {0}")]
-    Csv(#[from] CsvError),
-
-    #[error("JSON serialization error: {0}")]
-    JsonSerialization(#[from] serde_json::Error),
-}
-
+// Define a trait for PDF fetching
 #[async_trait]
-trait HtmlFetcher {
-    async fn fetch_html(&self, url: &str) -> Result<String, AppError>;
+trait PdfFetcher {
+    async fn fetch_pdf(&self, url: &str) -> Result<Vec<u8>, Box<dyn Error>>;
 }
 
-struct ReqwestHtmlFetcher {
+// Implement the PdfFetcher trait for a specific PDF fetcher
+struct PdfUrlFetcher {
     client: Client,
 }
 
-impl ReqwestHtmlFetcher {
+impl PdfUrlFetcher {
     fn new() -> Self {
         let client = Client::new();
         Self { client }
@@ -37,50 +21,40 @@ impl ReqwestHtmlFetcher {
 }
 
 #[async_trait]
-impl HtmlFetcher for ReqwestHtmlFetcher {
-    async fn fetch_html(&self, url: &str) -> Result<String, AppError> {
-        let response = self.client.get(url).send().await?;
-        let text_content = response.text().await?;
-        Ok(text_content)
+impl PdfFetcher for PdfUrlFetcher {
+    async fn fetch_pdf(&self, url: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+        // Fetch the PDF content using reqwest
+        let pdf_response = self.client.get(url).send().await?;
+        let pdf_bytes = pdf_response.bytes().await?;
+        Ok(pdf_bytes.to_vec())
     }
 }
 
-trait CsvParser {
-    fn parse_csv(&self, csv_text: &str) -> Result<(), AppError>;
+// Define a trait for PDF text extraction
+trait PdfTextExtractorService {
+    fn extract_text(&self, pdf_data: &[u8]) -> Result<String, Box<dyn Error>>;
 }
 
-struct CsvReader;
+// Implement the PdfTextExtractorService trait for a specific extractor
+struct PdfExtractor;
 
-impl CsvParser for CsvReader {
-    fn parse_csv(&self, csv_text: &str) -> Result<(), AppError> {
-        let mut csv_reader = ReaderBuilder::new().from_reader(Cursor::new(csv_text));
+impl PdfTextExtractorService for PdfExtractor {
+    fn extract_text(&self, pdf_data: &[u8]) -> Result<String, Box<dyn Error>> {
+        let pdf_text = pdf_extract::extract_text_from_mem(pdf_data)?;
 
-        // Print field names
-        if let Ok(field_names) = csv_reader.headers() {
-            println!("field names: {}", field_names.iter().collect::<Vec<_>>().join(" | "));
-        }
-
-        println!("---------------------");
-
-        // Print rows as dictionaries
-        for result in csv_reader.deserialize::<HashMap<String, serde_json::Value>>() {
-            let record = result?;
-            let json_object = serde_json::to_value(&record)?;
-            println!("{}", json_object);
-        }
-
-        Ok(())
+        Ok(pdf_text)
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), AppError> {
-    let html_fetcher = ReqwestHtmlFetcher::new();
-    let csv_parser = CsvReader;
-    let csv_url = "http://pythonscraping.com/files/MontyPythonAlbums.csv";
+async fn main() -> Result<(), Box<dyn Error>> {
+    let pdf_url = "http://pythonscraping.com/pages/warandpeace/chapter1.pdf";
+    let pdf_fetcher = PdfUrlFetcher::new();
+    let pdf_data = pdf_fetcher.fetch_pdf(pdf_url).await?;
 
-    let csv_text = html_fetcher.fetch_html(csv_url).await?;
-    csv_parser.parse_csv(&csv_text)?;
+    let pdf_extractor = PdfExtractor;
+    let pdf_text = pdf_extractor.extract_text(&pdf_data)?;
 
+    println!("{}", pdf_text);
     Ok(())
 }
